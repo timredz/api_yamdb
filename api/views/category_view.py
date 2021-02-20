fd8f422a0d922439fa84886b393828f766b7b23c
Анна Полякова <Sova040888@yandex.ru>
2021-02-20 10:10:38 +0300

    fix CategoryView, GenericView

--- api/views/category_view.py
+++ api/views/category_view.py
@@ -1,14 +1,18 @@
 from rest_framework import viewsets, permissions
-from rest_framework import filters
+from rest_framework import filters, mixins
 
 from api.models import Category
 from api.permissions import IsAdmin
 from api.serializers import CategorySerializer
 
 
-class CategoryViewSet(viewsets.ModelViewSet):
+class CategoryViewSet(mixins.CreateModelMixin,
+                      mixins.ListModelMixin,
+                      mixins.DestroyModelMixin,
+                      viewsets.GenericViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     # permission_classes = [ | IsAdmin]
     filter_backends = [filters.SearchFilter]
     search_fields = ['name']
+    lookup_field = 'slug'
